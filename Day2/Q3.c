e42abd42dbebdd89c1a5671b82424a09d0a522e9
Fasctor01729 <luo756feng@gmail.com>
2025-08-22 12:44:52 +0530

    commit

---
+++ Day2/Q3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    float length,width;
+    printf("Enter the length\n");
+    scanf("%f", &length);
+
+    printf("Enter the width\n");
+    scanf("%f", &width);
+
+    printf("The area of rectange is %f\n", length*width );
+    printf("The perimeter of rectangle is %f", 2*(length+width));
+
+    return 0;
+}
