0c44eaf01d1c185f1f083fa219f926649a0d6f0f
Pranav Singh <luo756feng@gmail.com>
2025-08-24 20:07:44 +0530

    Add files via upload

---
+++ Q8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int n, sum;
+
+    printf("Enter a positive. integer :");
+    scanf("%d",&n);
+
+    // using formula 
+    // sum = n(n+1)/2
+    sum = (n*(n+1)/2);
+    printf("THE SUM OF THE FIRST %D NATURAL NUMBERS IS :%d\n",n,sum);
+    
+  return 0;
+}
